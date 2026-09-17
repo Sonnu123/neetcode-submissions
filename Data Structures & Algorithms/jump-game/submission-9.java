1af20019298f973590a02781fdbaa9835d5e86e5
Surya Erramilli <160159935+Sonnu123@users.noreply.github.com>
2026-09-17 10:15:52 -0700

    Add: jump-game - submission-9

---
+++ Data Structures & Algorithms/jump-game/submission-9.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int farthest = nums[0];
+        for(int i = 1; i<nums.length; i++){
+            if(i<=farthest){
+                farthest = Math.max(farthest,i+nums[i]);
+            }
+        }
+        if(farthest >= nums.length-1){
+            return true;
+        }
+        return false;
+    }
+}
